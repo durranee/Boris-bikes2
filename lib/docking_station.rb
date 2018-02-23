122a973634024140011675ebcfacd937236ce9c2
durranee <durranee@hotmail.com>
2018-02-23 23:08:27 +0000

    cleaned up code a little

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,22 +2,30 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bikes
   DEFAULT_CAPACITY = 20
+
+  # Initialising DockingStation object with 20 bikes unless instructed otherwise
   def initialize (capacity = DEFAULT_CAPACITY)
     @bikes = []
     @capacity = capacity
   end
 
+# release_bike method releases bike and raise errors if there isn't any bikes
+# or there aren't any working bikes
   def release_bike
-    raise "Staaaahp! there is no bike" if empty?
-    fail "Sorry, bike broken" unless @bikes.last.working
+    raise 'Staaaahp! there is no bike' if empty?
+    fail 'Sorry, no working bikes available' unless @bikes.last.working
     @bikes.pop
   end
 
+# dock_bikes allows docking of bikes and if bike is broken then pushes it as
+# the first bike in the array
   def dock_bike(bike)
-    raise "there is no space" if full?
-    bike.working ? @bikes.push(bike) : @bikes.insert(0,bike)
+    raise 'there is no space' if full?
+    bike.working ? @bikes.push(bike) : @bikes.insert(0, bike)
   end
 
+# private methods full? and empty? to check if bike docking station
+# is full or empty
 private
   def full?
     @bikes.length >= @capacity
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-  before (:each) do
-    ds = DockingStation.new
-    # @fake_bike = double
-    # allow(@fake_bike).to receive(:working).and_return(true)
-    @fake_bike = double("fake_bike", :working => true)
+  before(:each) do
+    @fake_bike = double('fake_bike', :working => true)
   end
 
-  it { should respond_to(:release_bike)}
+  it { should respond_to(:release_bike) }
   it { should respond_to(:dock_bike) }
   it { should respond_to(:dock_bike).with(1).arguments }
 
@@ -16,9 +13,8 @@ describe DockingStation do
     expect(subject.dock_bike(@fake_bike).last).to eq(@fake_bike)
   end
 
-
   it 'release_bike should raise an error if docking station empty' do
-    expect { subject.release_bike }.to raise_error "Staaaahp! there is no bike"
+    expect { subject.release_bike }.to raise_error 'Staaaahp! there is no bike'
   end
 
   it 'dock_bike should raise an error if more than DEFAULT_CAPACITY' do
@@ -27,8 +23,7 @@ describe DockingStation do
       subject.dock_bike(@fake_bike)
     end
     # no more bikes can be docked so raise error
-    expect { subject.dock_bike(@fake_bike)}.to raise_error "there is no space"
-
+    expect { subject.dock_bike(@fake_bike) }.to raise_error 'there is no space'
   end
 
   it 'dock_bike should raise an error if its full' do
@@ -36,16 +31,14 @@ describe DockingStation do
     # it starts with 20 so subject is fine
     50.times { ds.dock_bike(@fake_bike) }
     # no more bikes can be docked so raise error
-    expect { ds.dock_bike(@fake_bike)}.to raise_error "there is no space"
+    expect { ds.dock_bike(@fake_bike) }.to raise_error 'there is no space'
   end
 
   it "doesn't release a bike if it is broken" do
     ds = DockingStation.new
     # broken fake bike scenario set up, one method called working returns false
-    @broken_fake_bike = double("broken_bike", :working => false)
-    ds.dock_bike(@broken_fake_bike)
-    expect { ds.release_bike }.to raise_error "Sorry, bike broken"
+    broken_fake_bike = double('broken_bike', :working => false)
+    ds.dock_bike(broken_fake_bike)
+    expect { ds.release_bike }.to raise_error 'Sorry, no working bikes available'
   end
-
-
 end
