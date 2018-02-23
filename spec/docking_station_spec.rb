c6a517733082bfe96befe866f0475f03ab35427a
durranee <durranee@hotmail.com>
2018-02-23 16:49:35 +0000

    used shorthands for method stubs and doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,8 +3,9 @@ require 'docking_station'
 describe DockingStation do
   before (:each) do
     ds = DockingStation.new
-    @fake_bike = double
-    allow(@fake_bike).to receive(:working).and_return(true)
+    # @fake_bike = double
+    # allow(@fake_bike).to receive(:working).and_return(true)
+    @fake_bike = double("fake_bike", :working => true)
   end
 
   it { should respond_to(:release_bike)}
@@ -12,13 +13,11 @@ describe DockingStation do
   it { should respond_to(:dock_bike).with(1).arguments }
 
   it 'checks if dock_bike actually takes a bike' do
-
     expect(subject.dock_bike(@fake_bike).last).to eq(@fake_bike)
   end
 
 
   it 'release_bike should raise an error if docking station empty' do
-    #20.times {ds.release_bike}
     expect { subject.release_bike }.to raise_error "Staaaahp! there is no bike"
   end
 
@@ -26,7 +25,7 @@ describe DockingStation do
     # it starts with 20 so subject is fine
     DockingStation::DEFAULT_CAPACITY.times do
       subject.dock_bike(@fake_bike)
-      end
+    end
     # no more bikes can be docked so raise error
     expect { subject.dock_bike(@fake_bike)}.to raise_error "there is no space"
 
@@ -42,8 +41,8 @@ describe DockingStation do
 
   it "doesn't release a bike if it is broken" do
     ds = DockingStation.new
-    @broken_fake_bike = double
-    allow(@broken_fake_bike).to receive(:working).and_return(false)
+    # broken fake bike scenario set up, one method callled working returns false
+    @broken_fake_bike = double("broken_bike", :working => false)
     ds.dock_bike(@broken_fake_bike)
     expect { ds.release_bike }.to raise_error "Sorry, bike broken"
   end
